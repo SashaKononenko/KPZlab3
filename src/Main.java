b1d58e3fcb4cfcf66b52717de98fe11eecc43c29
SashaKononenko <alex327021@gmail.com>
2017-06-16 01:03:18 +0300

    01

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by sasha on 27.04.17.
+ */
+public class Main {
+    static public void main(String argv[]) {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.showWindow();
+    }
+}
---
+++ test/TriangleTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alex3 on 6/16/2017.
+ */
+public class TriangleTest {
+    Triangle triangle = new Triangle(20,20,70,70,20,70);
+    @Test
+    public void measureParties() throws Exception {
+        assertEquals("Result:","Довжини сторін: \n" +
+                "70.71067811865476\n" +
+                "50.0\n" +
+                "50.0\n"
+                ,triangle.measureParties());
+    }
+
+    @Test
+    public void measurePerimetry() throws Exception {
+        assertEquals("Result:","Периметр:170.71067811865476\n",triangle.measurePerimetry());
+    }
+
+    @Test
+    public void measureArea() throws Exception {
+        assertEquals("Result:","Площа:1250.0\n",triangle.measureArea());
+    }
+
+}
